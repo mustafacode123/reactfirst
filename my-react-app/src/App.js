e106fff726cecafa643cb254bf6d6f4fe5170dce
musti <mmkayani786@gmail.com>
2020-10-22 20:55:37 +0100

    created TopMenu component

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import TopMenu from './componets/topmenu';
 
 function App() {
   return (
     <div className="App">
+      <TopMenu />
     </div>
   );
 }
